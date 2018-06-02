669fcc3b780d8a8934596983d4fc80f2475a6268
VIVAFROG <vivafrog@gmail.com>
2018-06-03 00:18:55 +0900

    6-2

--- src/App.js
+++ src/App.js
@@ -6,39 +6,14 @@ import Movie from './Movie';
 class App extends Component {
   //Render : componentWillMount() -> render() -> componentDidMount()
   //Update componentWillReciveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
-  state = {};
+
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        greeting: 'Hello!',
-        movies: [
-          {
-            title: 'Matrix',
-            poster: 'https://images-na.ssl-images-amazon.com/images/I/51k1epcewKL.jpg',
-          },
-          {
-            title: 'Full Metal Jacket',
-            poster: 'https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg',
-          },
-          {
-            title: 'Oldboy',
-            poster:
-              'https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg',
-          },
-          {
-            title: 'Star Wars',
-            poster:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png',
-          },
-          {
-            title: 'trainspotting',
-            poster:
-              'https://ia.media-imdb.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
-          },
-        ],
-      });
-    }, 5000);
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
