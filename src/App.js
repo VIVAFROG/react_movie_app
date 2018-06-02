f21320f72b7d7451b4a176cead7bd80b70b85fb8
VIVAFROG <vivafrog@gmail.com>
2018-06-03 01:37:59 +0900

    7-2

--- src/App.js
+++ src/App.js
@@ -38,7 +38,9 @@ class App extends Component {
   };
 
   _callApi = () => {
-    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
       .then(potato => potato.json())
       .then(json => json.data.movies)
       .catch(err => console.log(err));
