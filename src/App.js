bad64f89bafb95135a17e8fd1bccdd9f9ac16d81
VIVAFROG <vivafrog@gmail.com>
2018-06-02 22:10:09 +0900

    5-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
@@ -9,15 +11,14 @@ class App extends Component {
   //Render : componentWillMount() -> render() -> componentDidMount()
   //Update componentWillReciveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
   state = {
-      
+
   }
 
-  componentDidMount(){
+  componentDidMount() {
     setTimeout(() => {
       this.setState({
         greeting: 'Hello!',
-        movies: [
-          {
+        movies: [{
             title: "Matrix",
             poster: "https://images-na.ssl-images-amazon.com/images/I/51k1epcewKL.jpg"
           },
@@ -26,16 +27,16 @@ class App extends Component {
             poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
           },
           {
-            title:  "Oldboy",
-            poster:  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+            title: "Oldboy",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
           },
           {
-            title:   "Star Wars",
-            poster:   "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png"
+            title: "Star Wars",
+            poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png"
           },
           {
-            title:"trainspotting",
-            poster:"https://ia.media-imdb.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            title: "trainspotting",
+            poster: "https://ia.media-imdb.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
           }
         ]
       })
@@ -43,17 +44,27 @@ class App extends Component {
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) =>{
-      return  <Movie title={movie.title} poster={movie.poster} key={index}/>
-     })
-     return movies
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = {
+        movie.title
+      }
+      poster = {
+        movie.poster
+      }
+      key = {
+        index
+      }
+      />
+    })
+    return movies
   }
 
   render() {
-    return (   
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
-      </div>
+    return ( <
+      div className = "App" > {
+        this.state.movies ? this._renderMovies() : 'Loading'
+      } <
+      /div>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,40 +1,70 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
+import React, {
+    Component
+} from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-class Movie extends Component{
+class Movie extends Component {
     static propTypes = {
         title: PropTypes.string.isRequired,
         poster: PropTypes.string.isRequired
-    }
+    };
 
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )  
-    }
-
-}
-
-class MoviePoster extends Component{
-    static protoTypes={
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        return(
-            <img src={this.props.poster} />
-        )
+    render() {
+        return ( <
+            div >
+            <
+            MoviePoster poster = {
+                this.props.poster
+            }
+            /> <
+            h1 > {
+                this.props.title
+            } < /h1> <
+            /div>
+        );
     }
 }
+/*
+class MoviePoster extends Component {
+  static protoTypes = {
+    poster: PropTypes.string.isRequired
+  };
+  render() {
+    return <img src={this.props.poster} />;
+  }
+}
+*/
 
-function MoviePoser({poster}){
-    return(
-        <img src={this.props.poster} alt="Movie Poster"/>
+function Movie({
+    title,
+    poster
+}) {
+    return ( <
+        div >
+        <
+        MoviePoster poster = {
+            this.props.poster
+        }
+        /> <
+        h1 > {
+            this.props.title
+        } < /h1> <
+        /div>        
     )
 }
 
+function MoviePoster({
+    poster
+}) {
+    return <img src = {
+        poster
+    }
+    alt = "Movie Poster" / > ;
+}
 
-export default Movie
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+
+export default Movie;
